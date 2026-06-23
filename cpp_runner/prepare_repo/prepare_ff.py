a5006d64d742b4e4e90f93c44582a005e9e986bd
Johannes Wehrstein <johannes.wehrstein@cs.tu-darmstadt.de>
2026-06-23 15:18:15 +0000

    fix prepare ff

--- cpp_runner/prepare_repo/prepare_ff.py
+++ cpp_runner/prepare_repo/prepare_ff.py
@@ -2,24 +2,29 @@ from cpp_runner.prepare_repo.load_snapshot_and_prepare import PrepareContext
 
 
 def prepare_ff_plan(ctx: PrepareContext) -> str:
+    usecase_args = {
+        **ctx.usecase_prepare_args,
+    }
+
     return ctx.prepare_workspace_provider.prepare(
-        add_thread_pool_to_query_impl=False,
         only_query_md=True,
-        add_sample_trace=ctx.add_sample_trace,
         write_non_tracked_only=ctx.write_non_tracked_only,
         only_from_cache=ctx.only_from_cache,
         do_not_cache=ctx.do_not_cache,
-        usecase_args=ctx.usecase_prepare_args,
+        usecase_args=usecase_args,
     )
 
 
 def prepare_base_ff(ctx: PrepareContext) -> str:
+
+    usecase_args = {
+        **ctx.usecase_prepare_args,
+    }
+
     return ctx.prepare_workspace_provider.prepare(
-        add_thread_pool_to_query_impl=False,
         only_query_md=False,
-        add_sample_trace=ctx.add_sample_trace,
         write_non_tracked_only=ctx.write_non_tracked_only,
         only_from_cache=ctx.only_from_cache,
         do_not_cache=ctx.do_not_cache,
-        usecase_args=ctx.usecase_prepare_args,
+        usecase_args=usecase_args,
     )
