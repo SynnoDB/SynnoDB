e8d88781638a12c1d420e975c8434ae2a95c4c56
Jigao Luo <jigao.luo@outlook.com>
2026-06-23 17:18:48 +0200

    first draft of BFF

--- cpp_runner/api/bff/ingest_api.cpp
+++ cpp_runner/api/bff/ingest_api.cpp
@@ -3,21 +3,26 @@
 #include "../hotpatch/plugin_base.hpp"
 
 static const IngestApi INGEST = {
-    .write_from_parquet = &write_bff_from_parquet,
-    .open_dataset = &open_bff_dataset,
-    .close_dataset = &close_bff_dataset,
-    .load_footer = &load_bff_footer,
-    .cached_footer = &cached_bff_footer,
-    .describe_footer = &describe_bff_footer,
-    .invalidate_footer_cache = &invalidate_bff_footer_cache,
-    .open_table = &open_bff_table,
-    .close_table = &close_bff_table,
-    .describe_table = &describe_bff_table,
-    .describe_row_group = &describe_bff_row_group,
-    .describe_page = &describe_bff_page,
-    .read_row_group = &read_bff_row_group,
-    .read_page = &read_bff_page,
-    .release_buffer = &release_bff_buffer,
+    .write = {
+        .write_from_parquet = &write_bff_from_parquet,
+        .write_from_parquet_tables = &write_bff_from_parquet_tables,
+    },
+    .read = {
+        .open_dataset = &open_bff_dataset,
+        .close_dataset = &close_bff_dataset,
+        .load_footer = &load_bff_footer,
+        .cached_footer = &cached_bff_footer,
+        .describe_footer = &describe_bff_footer,
+        .invalidate_footer_cache = &invalidate_bff_footer_cache,
+        .open_table = &open_bff_table,
+        .close_table = &close_bff_table,
+        .describe_table = &describe_bff_table,
+        .describe_row_group = &describe_bff_row_group,
+        .describe_page = &describe_bff_page,
+        .read_row_group = &read_bff_row_group,
+        .read_page = &read_bff_page,
+        .release_buffer = &release_bff_buffer,
+    },
 };
 
 extern "C" __attribute__((visibility("default")))
